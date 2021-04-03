568151a2e1233e742d78b98cf68c5b2e1fb55e97
Łukasz Tyczka <lukasz@tyczka.net>
2021-04-03 12:32:09 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import Dashboard from './components/Dashboard/Dashboard';
+import Preference from './components/Preferences/Preferences';
+import {
+    BrowserRouter,
+    BrowserRouter as Router,
+    Route,
+    Switch,
+} from 'react-router-dom';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello</h1>
+        <div className="wrapper">
+            <h1>Application</h1>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/preferences">
+                        <Preference />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
         </div>
     );
 }
---
+++ src/components/Preferences/Preferences.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Preferences() {
+    return (
+        <div>
+            <h2>Preference</h2>
+        </div>
+    );
+}
+
+export default Preferences;

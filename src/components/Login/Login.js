a51da211633584ccb51558b2defd27a21983d573
Łukasz Tyczka <lukasz@tyczka.net>
2021-04-03 13:25:26 +0200

    create API and use it to make login works

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
 import './login.css';
 
-function Login() {
+async function loginUser(credentials) {
+    return fetch('http://localhost:8080/login', {
+        method: 'Post',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials),
+    }).then((data) => data.json());
+}
+
+function Login({ setToken }) {
+    const [userName, setUserName] = useState();
+    const [password, setPassword] = useState();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const token = await loginUser({
+            userName,
+            password,
+        });
+        setToken(token);
+    };
     return (
         <div className="login-wrapper">
             <h1>Please Log In</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <lable>
                     <p>Username</p>
                     <input type="text" />
                 </lable>
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input
+                        type="password"
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
                 </label>
                 <div>
-                    <button type="submit">Submit</button>
+                    <button
+                        type="submit"
+                        onChange={(e) => setPassword(e.target.value)}>
+                        Submit
+                    </button>
                 </div>
             </form>
         </div>
@@ -23,3 +52,7 @@ function Login() {
 }
 
 export default Login;
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired,
+};
